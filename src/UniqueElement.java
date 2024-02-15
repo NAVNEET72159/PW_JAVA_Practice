c5dd5a644a2849703b71f5f116bba187a922b4a6
Navneet Shandilya <30992372+NAVNEET72159@users.noreply.github.com>
2024-02-15 19:57:29 +0530

    commit

--- src/UniqueElement.java
+++ src/UniqueElement.java
@@ -1,6 +1,6 @@
 public class UniqueElement {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5, 1, 2, 3, 4};
+        int[] arr = {1, 2, 3, 4, 1, 2, 4};
         System.out.println(uniqueElement(arr));
     }
     public static int uniqueElement(int[] arr) {
